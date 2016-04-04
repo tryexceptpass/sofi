d515b7bd378c44c3b9dcf67f604aec264bed4dc6
tryexceptpass <@tryexceptpass>
2016-04-04 19:15:57 -0400

    Added Element object - closes #1

---
+++ sofi/element.py
@@ -0,0 +1,23 @@
+class Element(object):
+    """Base HTML tag element"""
+
+    def __init__(self, cl=None, ident=None, style=None):
+        """Create a base element where cl is a space separated class attribute,
+           ident is the element id and style is a CSS style string"""
+
+        self.cl = cl
+        self.ident = ident
+        self.style = style
+
+        self.children = list()
+
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return "Element<cl='" + self.cl + "',ident='" + self.ident + "',children=" + str(len(self.chidren)) + ">"
+
+    def additem(self, item):
+        if item is not None:
+            self.children.append(item)
