e4a1f242ef29a395d532328bb31a88be30d03934
tryexceptpass <@tryexceptpass>
2016-08-09 17:45:29 -0400

    Added anchor tests

---
+++ sofi/tests/anchor_test.py
@@ -0,0 +1,11 @@
+from .. import Anchor
+
+def test_basic():
+    assert(str(Anchor()) == "<a href=\"#\"></a>")
+
+def test_text_href():
+    assert(str(Anchor("text", "http://localhost/abc")) == "<a href=\"http://localhost/abc\">text</a>")
+
+def test_custom_class_ident_and_style():
+    assert(str(Anchor("text", cl='abclass', ident='123', style="font-size:0.9em;"))
+           == "<a id=\"123\" class=\"abclass\" href=\"#\" style=\"font-size:0.9em;\">text</a>")
