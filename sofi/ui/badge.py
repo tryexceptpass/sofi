56a170dfa72848d24271c4481910a357e0c077b8
jigarmistry <jigarmistry24@gmail.com>
2016-08-23 20:19:57 +0530

    Simplify Badge Class !  - Plz Review

--- sofi/ui/badge.py
+++ sofi/ui/badge.py
@@ -1,4 +1,5 @@
 from .element import Element
+from .span import Span
 
 class Badge(Element):
     """Implement Badges"""
@@ -6,36 +7,14 @@ class Badge(Element):
     def __init__(self, text=None, ident=None, style=None, attrs=None):
         super().__init__(ident=ident, style=style, attrs=attrs)
 
-        if text:
-            self.children.append(text)
+        self.text = text
+        self.ident = ident
+        self.style = style
+        self.attrs = attrs
 
     def __repr__(self):
-        return "<Span class=\"badge\">"
+        return "<Badge text='" + self.text + "'>"
 
     def __str__(self):
-        output = [ "<span" ]
-
-        output.append(" class=\"badge\"")
-
-        if self.ident:
-            output.append(" id=\"")
-            output.append(self.ident)
-            output.append("\"")
-        
-        if self.style:
-            output.append(" style=\"")
-            output.append(self.style)
-            output.append("\"")
-
-        if self.attrs:
-            for k in self.attrs.keys():
-                output.append(' ' + k + '="' + self.attrs[k] + '"')
-
-        output.append(">")
-
-        for child in self.children:
-            output.append(str(child))
-
-        output.append("</span>")
-
-        return "".join(output)
+        return str(Span(text=self.text,cl="badge",ident=self.ident,
+                    style=self.style,attrs=self.attrs))
