e4d8ccc48899c8ad2d12fcfe2850315919e9a769
tryexceptpass <tryexceptpass@users.noreply.github.com>
2016-09-15 17:30:56 -0400

    Forgot to update tests since last changes to Button, now fixed.

--- test/button_test.py
+++ test/button_test.py
@@ -1,23 +1,23 @@
 from sofi.ui import Button
 
 def test_basic():
-    assert(str(Button("text")) == "<button class=\"btn btn-default\">text</button>")
+    assert(str(Button("text")) == "<button class=\"btn btn-default\" type=\"button\">text</button>")
 
 def test_danger_severity_large():
-    assert(str(Button("text", "danger", "large")) == "<button class=\"btn btn-danger btn-lg\">text</button>")
+    assert(str(Button("text", "danger", "large")) == "<button class=\"btn btn-danger btn-lg\" type=\"button\">text</button>")
 
 def test_info_severity_small():
-    assert(str(Button("text", "info", "small")) == "<button class=\"btn btn-info btn-sm\">text</button>")
+    assert(str(Button("text", "info", "small")) == "<button class=\"btn btn-info btn-sm\" type=\"button\">text</button>")
 
 def test_primary_severity_xsmall():
-    assert(str(Button("text", "primary", "xsmall")) == "<button class=\"btn btn-primary btn-xs\">text</button>")
+    assert(str(Button("text", "primary", "xsmall")) == "<button class=\"btn btn-primary btn-xs\" type=\"button\">text</button>")
 
 def test_success_severity():
-    assert(str(Button("text", "success")) == "<button class=\"btn btn-success\">text</button>")
+    assert(str(Button("text", "success")) == "<button class=\"btn btn-success\" type=\"button\">text</button>")
 
 def test_warning_severity():
-    assert(str(Button("text", "warning")) == "<button class=\"btn btn-warning\">text</button>")
+    assert(str(Button("text", "warning")) == "<button class=\"btn btn-warning\" type=\"button\">text</button>")
 
 def test_custom_class_ident_and_style():
     assert(str(Button("text", cl='abclass', ident='123', style="font-size:0.9em;"))
-           == "<button id=\"123\" class=\"btn btn-default abclass\" style=\"font-size:0.9em;\">text</button>")
+           == "<button id=\"123\" class=\"btn btn-default abclass\" type=\"button\" style=\"font-size:0.9em;\">text</button>")
