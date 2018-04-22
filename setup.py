f651ba002e172edf49c53dcd96af155f5bdc0d8a
tryexceptpass <@tryexceptpass>
2018-04-22 10:48:36 -0400

    Updated setup info and version

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     author_email="cmedina@tryexceptpass.org",
 
     name="sofi",
-    version="0.3.0",
+    version="0.3.2",
 
-    description="WebSocket server and protocol for GUI layers based on WebSockets",
+    description="Desktop and Web GUI framework based on WebSockets",
     long_description="Sofi is a Python 3 WebSocket server and protocol that works with clients to generate GUI applications. For websites, it generates the necessary HTML and JavaScript needed to produce a single-page application with event listeners. In conjunction with sofi-unity3d, it can drive the Unity3D game engine.",
 
     url="https://github.com/tryexceptpass/sofi",
@@ -25,7 +25,13 @@ setup(
 
     license="MIT",
     classifiers=['License :: OSI Approved :: MIT License',
-                 # ADD MORE CLASSIFIERS!
+                 'Framework :: AsyncIO',
+                 'Topic :: Internet :: WWW/HTTP',
+                 'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+                 'Topic :: Multimedia',
+                 'Topic :: Multimedia :: Graphics',
+                 'Topic :: Software Development :: User Interfaces',
+
                  'Development Status :: 4 - Beta',
                  ],
     keywords=['websockets', 'javascript', 'bootstrap', 'gui', 'unity3d']
